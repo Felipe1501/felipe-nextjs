6cc146f28a599f9e6b2df38a5bb89c04e380e4ce
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-06 12:27:10 -0300

    aula 92 fix: using components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <header>
+            <h1>Fudamentos de NextJs & React</h1>
+        </header>
+    )
+}
---
+++ src/pages/example.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+
+export default function Example(){
+    return (
+        <>
+        <Header/>
+        <Header/>
+        </>
+
+    )
+}
