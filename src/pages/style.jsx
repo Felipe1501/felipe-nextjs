2e87957e3691a4a9a6dab8eed59b6e71045eda57
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-06 12:20:04 -0300

    aula 91 fix: CSS modules

---
+++ src/pages/style.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Style.module.css';
+
+export default function Style(){
+    return (
+        <div className={styles.vermelho}>
+
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+}
