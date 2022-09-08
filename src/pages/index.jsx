17fc611c470647e56066f551d16ed8cb56524a3f
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-08 10:25:00 -0300

    aula 103 fix: changing component colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,12 +9,12 @@ export default function Home(){
                 flexWrap: 'wrap',
                 height: '100vh'
             }}>
-                <Navegation text="Style" destino="/style"/>
+                <Navegation text="Style" destino="/style" cor="#E64F3C"/>
                 <Navegation text="Example" destino="/example" cor="#9400d3"/>
                 <Navegation text="JSX" destino="/jsx" cor="crimson"/>
                 <Navegation text="Navegação #01" destino="/navegation" cor="green"/>
                 <Navegation text="Navegação #02" destino="/client/sp-2/1501" cor="purple"/>
-                <Navegation text="Componente com Estado" destino="/estado" cor="orange"/>
+                <Navegation text="Componente com Estado" destino="/estado" cor="#3366F2"/>
                 <Navegation text="Integração com API #01" destino="/integration_1" cor="red"/>
             </div>
         )
